d3e55bd98d721fca9f7db002e03f4fda74a02e54
badarirao <badari.rao@gmail.com>
2022-01-18 17:09:56 +0900

    hello

--- scanner_gui.py
+++ scanner_gui.py
@@ -6,6 +6,7 @@
 #
 # WARNING! All changes made in this file will be lost!
 
+#hello
 from PyQt5 import QtCore, QtGui, QtWidgets
 from pyqtgraph import ImageView, PlotItem, PlotWidget, ViewBox, mkPen
 
